2aa8a1c2e6d4aa2975840b0c3e8753384c12d254
tianqin053-ux <tianqin053@gmail.com>
2026-09-11 17:37:05 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,22 @@
-plugins { id("com.android.application") }android {    namespace = "com.example.duofold6"    compileSdk = 35    defaultConfig {        applicationId = "com.example.duofold6"        minSdk = 30        targetSdk = 35        versionCode = 1        versionName = "0.1"    }    buildTypes { release { isMinifyEnabled = false } }}
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.duofold6"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.duofold6"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
