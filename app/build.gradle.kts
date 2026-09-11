5de5c1fb6e1076e498d5de28724c5657b9ba7570
tianqin053-ux <tianqin053@gmail.com>
2026-09-11 15:30:18 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,1 @@
+plugins { id("com.android.application") }android {    namespace = "com.example.duofold6"    compileSdk = 35    defaultConfig {        applicationId = "com.example.duofold6"        minSdk = 30        targetSdk = 35        versionCode = 1        versionName = "0.1"    }    buildTypes { release { isMinifyEnabled = false } }}
